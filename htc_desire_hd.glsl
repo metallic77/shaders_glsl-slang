ec9a4cbeb09f9c4349417bcb538696ec21e9564e
metallic77 <43163462+metallic77@users.noreply.github.com>
2023-05-04 21:59:39 +0300

    Update htc_desire_hd.glsl

--- htc_desire_hd.glsl
+++ htc_desire_hd.glsl
@@ -9,10 +9,8 @@
     any later version.
 
 */
+#pragma parameter SCANLINE_WEIGHT "Scanline Brightness" 0.5 0.0 1.0 0.1
 
-#pragma parameter MASK "Mask brightness" 0.70 0.0 1.0 0.05
-#pragma parameter SCANLINE_WEIGHT "Scanline weight" 0.5 0.0 1.0 0.05
-#pragma parameter BLOOM "Bloom factor" 0.7 0.0 1.5 0.05
 
 #define pi 6.28306
 
@@ -43,6 +41,8 @@ uniform vec2 TextureSize;
 varying vec2 TEX0;
 varying float omega;
 
+
+
 #if defined(VERTEX)
 uniform mat4 MVPMatrix;
 attribute vec4 VertexCoord;
@@ -54,33 +54,29 @@ void main()
 {
 	TEX0 = TexCoord*1.0001;                    // 1 cycle
 	gl_Position = MVPMatrix * VertexCoord;     // 1 cycle
-        omega = pi*TextureSize.y;                  // 1 cycle, total 3
+        omega = TEX0.y * pi* TextureSize.y;        // 2 cycles
 }
 #elif defined(FRAGMENT)
 
 uniform sampler2D Texture;
 
+#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
 
 float CalcScanLine(float dy)
 {
-        return 1.5 + SCANLINE_WEIGHT*sin(dy*omega)*0.5  ;  // 3 cycles
+        return 0.5 + sin(omega)*0.5  ;  // 1 cycles
 }
 
 void main()
 {
-      		vec2 pos = TEX0;
+      	vec2 pos = TEX0;
 		vec2 OGL2pos = pos * TextureSize;          // 1 cycle    // this could be moved to vertex ?
 
-		float tempY = floor(OGL2pos.y) + 0.5;      // 1 cycle
+		float tempY = floor(OGL2pos.y) + 0.5;      // 2 cycles? floor too
 		float yCoord = tempY / TextureSize.y;      // 1 cycle
 		float dy = OGL2pos.y - tempY;              // 1 cycle	  // up to here move to vertex and out yCoord and dy?
-		float signY = sign(dy);                    // 1 cycle ??
-		dy = dy * dy;                              // 1 cycle
-		dy = dy * dy;				   // 1 cycle
-		dy *= 8.0;				   // 1 cycle
-		dy /= TextureSize.y;                       // 1 cycle
-		dy *= signY; 				   // 1 cycle
-		vec2 tc = vec2(pos.x, yCoord + dy); 	// 10 cycles total for tc?
+		float uy = yCoord + (dy*dy*dy)*SourceSize.w; //4 cycles                             
+		vec2 tc = vec2(pos.x, uy); 	// 10 cycles total for tc?
 
 // Vertex and Fragment processors calculate vec4 multiply/add etc operations in 1 cycle. 
 // Either you multiply a vec4 to a vec4 or a float to a float will cost 1 cycle
@@ -89,15 +85,16 @@ void main()
 vec3 colour = texture2D(Texture, tc).rgb;
 
 //SCANLINES
-		float scanLineWeight = CalcScanLine(TEX0.y);   // costed 3 cycles
-		scanLineWeight *= BLOOM; 		       // 1 cycle
-		colour *= scanLineWeight;  // 7 cycles for scanlines   // 1 cycle, total 14 for filter and scanlines. If we take out filter we gain about 70% speedup!
+		float scanLineWeight = CalcScanLine(TEX0.y);   // costed 1 cycles
+		scanLineWeight = mix(scanLineWeight,1.0,SCANLINE_WEIGHT); //1 cycles? probably more		       // 1 cycle
+		colour *= scanLineWeight; // 1 cycle 
 
 		//float whichMask = fract((gl_FragCoord.x*1.0001) * 0.5);    
 		//vec3 mask;
 		//if (whichMask < 0.5) mask = vec3(MASK);		    
 		//else mask = vec3(1.0);				   
-									
+		//colour *= mask;							
+		
 		gl_FragColor = vec4(colour, 1.0);
 
 	}
