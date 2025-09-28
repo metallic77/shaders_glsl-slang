a9d4cfd63b9db9a3c589c52b22f01b71ba81f5eb
metallic77 <43163462+metallic77@users.noreply.github.com>
2025-09-28 18:20:44 +0300

    Update crt-consumer-v0.2.glsl

--- crt-consumer-old/crt-consumer-v0.2.glsl
+++ crt-consumer-old/crt-consumer-v0.2.glsl
@@ -3,14 +3,13 @@
 // Parameter lines go here:
 
 #pragma parameter WARP "Curvature" 0.04 0.0 0.12 0.01
-#pragma parameter CONVX "Convergence X" 0.35 -1.0 1.0 0.05
-#pragma parameter CONVY "Convergence Y" -0.15 -1.0 1.0 0.05
+#pragma parameter CONVX "Convergence X" 0.6 -1.0 1.0 0.05
+#pragma parameter CONVY "Convergence Y" -0.1 -1.0 1.0 0.05
 #pragma parameter Shadowmask "Mask:0:cgwg,1-2:lottes,3:gw,4:slot " 0.0 -1.0 4.0 1.0
 #pragma parameter MaskDark "Mask Dark" 0.5 0.0 2.0 0.1
 #pragma parameter MaskLight "Mask Light" 1.5 0.0 2.0 0.1
-#pragma parameter BRIGHTBOOST1 "Bright boost" 1.1 0.0 2.0 0.05
-#pragma parameter GAMMA_OUT "Gamma Out" 1.8 0.0 4.0 0.1
-#pragma parameter SATURATION "Saturation" 1.2 0.0 2.0 0.05
+#pragma parameter BRIGHTBOOST1 "Boost Dark" 1.25 0.0 2.0 0.05
+#pragma parameter SATURATION "Saturation" 1.0 0.0 2.0 0.05
 
 
 
@@ -38,6 +37,7 @@
 COMPAT_ATTRIBUTE vec4 VertexCoord;
 COMPAT_ATTRIBUTE vec4 TexCoord;
 COMPAT_VARYING vec4 TEX0;
+COMPAT_VARYING vec2 scale;
 
 uniform mat4 MVPMatrix;
 uniform COMPAT_PRECISION int FrameDirection;
@@ -55,6 +55,7 @@ void main()
 {
    gl_Position = MVPMatrix * VertexCoord;
    TEX0.xy = TexCoord.xy*1.0001;
+   scale = TextureSize/InputSize;
 }
 
 #elif defined(FRAGMENT)
@@ -87,11 +88,11 @@ uniform COMPAT_PRECISION vec2 TextureSize;
 uniform COMPAT_PRECISION vec2 InputSize;
 uniform sampler2D Texture;
 COMPAT_VARYING vec4 TEX0;
+COMPAT_VARYING vec2 scale;
 
 // compatibility #defines
 #define Source Texture
 #define vTexCoord TEX0.xy
-#define iChannel0 Texture
 #define iTime (float(FrameCount) / 60.0)
 
 #define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
@@ -103,7 +104,6 @@ uniform COMPAT_PRECISION float WARP;
 uniform COMPAT_PRECISION float CONVX;
 uniform COMPAT_PRECISION float CONVY;
 uniform COMPAT_PRECISION float SATURATION;
-uniform COMPAT_PRECISION float GAMMA_OUT;
 uniform COMPAT_PRECISION float BRIGHTBOOST1;
 uniform COMPAT_PRECISION float Shadowmask;
 uniform COMPAT_PRECISION float MaskDark;
@@ -118,7 +118,6 @@ uniform COMPAT_PRECISION float MaskLight;
 #define CONVY -0.15
 #define SATURATION 1.2 
 #define BRIGHTBOOST1 1.1 
-#define GAMMA_OUT 2.2
 #define Shadowmask 0.0
 #define MaskDark 0.5
 #define MaskLight 1.5
@@ -229,14 +228,14 @@ vec3 mask(vec2 x)
 }
 
 
-vec3 saturation (vec3 textureColor)
+vec3 saturation (vec3 Color)
 {
 
     vec3 luminanceWeighting = vec3(0.3,0.6,0.1);
-    float luminance = dot(textureColor.rgb, luminanceWeighting);
+    float luminance = dot(Color, luminanceWeighting);
     vec3 greyScaleColor = vec3(luminance);
 
-    vec3 res = vec3(mix(greyScaleColor, textureColor.rgb, SATURATION));
+    vec3 res = vec3(mix(greyScaleColor, Color, SATURATION));
     return res;
 }
 
@@ -247,35 +246,36 @@ mat3 vign( float l )
     vec2 vpos = vTexCoord * (TextureSize.xy / InputSize.xy);
     vpos *= 1.0 - vpos.xy;
     float vig = vpos.x * vpos.y * 40.0;
-    vig = min(pow(vig, 0.2), 1.0); 
+    vig = min(pow(vig, 0.15), 1.0); 
 
     return mat3(vig, 0, 0,
                  0,   vig, 0,
                  0,    0, vig);
 
 }
-
 void main()
 {
-    vec2 uv = Warp(TEX0.xy*(TextureSize.xy/InputSize.xy))*(InputSize.xy/TextureSize.xy);
+    vec2 uv = Warp(TEX0.xy*(scale))/scale;
     vec2 OGL2Pos = uv * SourceSize.xy;
-    vec2 fp = fract(OGL2Pos);
+    vec2 fp = fract(OGL2Pos-0.5);
     // Take multiple samples to displace different color channels
-    vec3 sample1 = texture(iChannel0, vec2(uv.x-CONVX/1000.0,uv.y-CONVY/1000.0)).rgb; 
-    vec3 sample2 = texture(iChannel0, uv).rgb;
-    vec3 sample3 = texture(iChannel0, vec2(uv.x+CONVX/1000.0,uv.y+CONVY/1000.0)).rgb;
+    vec3 sample1 = COMPAT_TEXTURE(Source, vec2(uv.x-CONVX/1000.0,uv.y-CONVY/1000.0)).rgb; 
+    vec3 sample2 = COMPAT_TEXTURE(Source, uv).rgb;
+    vec3 sample3 = COMPAT_TEXTURE(Source, vec2(uv.x+CONVX/1000.0,uv.y+CONVY/1000.0)).rgb;
  
-    vec3 color = vec3(0.5*sample1.r+0.5*sample2.r, 0.25*sample1.g+0.5*sample2.g+0.25*sample3.g, 0.5*sample2.b+0.5*sample3.b);   
+    vec3 color = vec3(0.5*sample1.r+0.5*sample2.r, 
+                     0.25*sample1.g+0.5*sample2.g+0.25*sample3.g, 
+                                    0.5*sample2.b+ 0.5*sample3.b);   
     color*=color;;
-    float lum=color.r*0.3 + color.g*0.6 + color.b*0.1;
+    float lum = dot(vec3(0.3,0.6,0.1),color);
 
-    color=color*scanLine(fp.y,lum)+color*scanLine(1.0-fp.y,lum);
-    color*=mask(gl_FragCoord.xy*1.0001);
+    color = color*scanLine(fp.y,lum) + color*scanLine(1.0-fp.y,lum);
+    color *= mask(gl_FragCoord.xy*1.0001);
 
-    color=pow(color,vec3(1.0/GAMMA_OUT,1.0/GAMMA_OUT,1.0/GAMMA_OUT)); 
-    color*=mix(1.0, BRIGHTBOOST1, lum);    
+    color = sqrt(color); 
+    color *= mix(BRIGHTBOOST1, 1.0, lum);    
 
-    if (SATURATION != 1.0) color = saturation(color);
+    color = saturation(color);
     color*=vign(lum);
 
     FragColor = vec4(color,1.0);
